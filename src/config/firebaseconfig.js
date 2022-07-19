08a823790df274fe69a6c77a6adc11caaa8ca6fc
Frank Botos <frankbotos@hotmail.com>
2022-07-19 06:25:34 -0400

    added environment variables

--- src/config/firebaseconfig.js
+++ src/config/firebaseconfig.js
@@ -2,14 +2,14 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAF3Pm2rU_FutDc8lSr4wXGFiBjIKIPXl4",
-  authDomain: "trelloclone-77c96.firebaseapp.com",
-  databaseURL: "https://trelloclone-77c96-default-rtdb.firebaseio.com",
-  projectId: "trelloclone-77c96",
-  storageBucket: "trelloclone-77c96.appspot.com",
-  messagingSenderId: "884723740290",
-  appId: "1:884723740290:web:375320675dddd99bd74e9b",
-  measurementId: "G-JTYC3H63FJ",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
